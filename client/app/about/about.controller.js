dd7b243127671799c19f92a9f15bcb8b5b80160c
mziyambi <mziyam@mtc.ca.gov>
2017-03-08 12:42:15 -0800

    [UI] FAQs and About page updates

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -1,16 +1,22 @@
 'use strict';
-(function(){
+(function() {
 
-class AboutComponent {
-  constructor() {
-    this.message = 'Hello';
-  }
-}
+    class AboutComponent {
+        constructor($scope) {
+            this.message = 'Hello';
+            $scope.status = {
+                isCustomHeaderOpen: false,
+                isFirstOpen: true,
+                isFirstDisabled: false
+            };
 
-angular.module('capvmtApp')
-  .component('about', {
-    templateUrl: 'app/about/about.html',
-    controller: AboutComponent
-  });
+        }
+    }
+
+    angular.module('capvmtApp')
+        .component('about', {
+            templateUrl: 'app/about/about.html',
+            controller: AboutComponent
+        });
 
 })();
